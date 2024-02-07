ca4394da4f2fc982ccca686eec29c4e2f89f7d30
xUnderGame <unaigiz1@gmail.com>
2024-02-07 15:11:14 +0100

    Database now loads correctly

--- Assets/Scripts/SQLiteTest.cs
+++ Assets/Scripts/SQLiteTest.cs
@@ -1,10 +1,12 @@
 using System.Data;
 using Mono.Data.SqliteClient;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SQLiteTest : MonoBehaviour
 {
-    private readonly string path = "URI=file:Assets/Database/Pecados.db";
+    public Text debugText;
+    private readonly string path = $"URI=file:{Application.streamingAssetsPath}/Database/Pecados.db";
     private IDbConnection connection;
     private IDbCommand command;
     private IDataReader reader;
@@ -16,14 +18,21 @@ public class SQLiteTest : MonoBehaviour
         connection = new SqliteConnection(path);
         connection.Open();
 
+        Debug.Log("Connection open");
         command = connection.CreateCommand();
-        command.CommandText = "SELECT * FROM sqlite_master";
+        command.CommandText = "SELECT * FROM pecados";
 
+        Debug.Log("Executing command");
         reader = command.ExecuteReader();
 
         while (reader.Read())
         {
-            Debug.LogWarning(reader.GetString(0));
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                var result = reader.GetString(i);
+                debugText.text += result + ", ";
+                Debug.Log(result);
+            }
         }
     }
 }
