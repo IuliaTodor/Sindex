8655c562d113ecbcc25fde03906f62551ca60913
iulia.todor.7e6 <iulia.todor.7e6@itb.cat>
2024-03-05 17:37:04 +0100

    La animación funciona mejor, pero raro

--- Assets/Scripts/ToggleAnimation.cs
+++ Assets/Scripts/ToggleAnimation.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,22 +7,47 @@ using UnityEngine.InputSystem;
 public class ToggleAnimation : MonoBehaviour
 {
     [SerializeField] private InputAction pressed;
-    void Update()
+    private Action<InputAction.CallbackContext> performHandler;
+    private Action<InputAction.CallbackContext> cancelHandler;
+
+    private void Awake()
+    {
+        pressed.Enable();
+
+        void performed(InputAction.CallbackContext _) { StartCoroutine(PlayAnimation()); }
+        performHandler = performed;
+
+        void canceled(InputAction.CallbackContext _) { }
+        cancelHandler = canceled;
+
+        //Cuando pressed es activado llama a la corutina. No le pasamos ninguna variable 
+        pressed.performed += performHandler;
+        //El objeto ya no rota cuando soltamos pressed
+        pressed.canceled += cancelHandler;
+    }
+
+    private void OnDisable()
+    {
+        pressed.performed -= performHandler;
+        pressed.canceled -= cancelHandler;
+    }
+
+    private IEnumerator PlayAnimation()
     {
         RaycastHit hit;
         Ray ray = new Ray(transform.position, transform.forward);
-        if (Input.GetKey(KeyCode.Mouse0))
+        Debug.Log("Mouse");
+        //Debug.DrawRay(ray.origin, ray.direction * 10f, Color.red, 2f);
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
         {
-            Debug.Log("Mouse");
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            Debug.Log("Raycast");
+            if (hit.collider.gameObject.CompareTag("3DModel"))
             {
-                Debug.Log("Raycast");
-                if (hit.collider.gameObject.CompareTag("3DModel"))
-                {
-                    Debug.Log("Trigger");
-                    hit.collider.gameObject.GetComponent<Animator>().SetTrigger("isTouched");
-                }
+                Debug.Log("Trigger");
+                hit.collider.gameObject.GetComponent<Animator>().SetTrigger("isTouched");
             }
         }
+
+        yield return null;
     }
 }
