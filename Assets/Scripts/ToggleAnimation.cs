e87a7fde41b9cfc8a02954c3a76fe8c1d3a4a123
iulia.todor.7e6 <iulia.todor.7e6@itb.cat>
2024-03-05 16:47:04 +0100

    solo ha cambiado una línea, pero la build depende de este pull

--- Assets/Scripts/ToggleAnimation.cs
+++ Assets/Scripts/ToggleAnimation.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ToggleAnimation : MonoBehaviour
 {
+    [SerializeField] private InputAction pressed;
     void Update()
     {
         RaycastHit hit;
         Ray ray = new Ray(transform.position, transform.forward);
-        if (Input.GetMouseButton(0))
+        if (Input.GetKey(KeyCode.Mouse0))
         {
+            Debug.Log("Mouse");
             if (Physics.Raycast(ray, out hit, Mathf.Infinity))
             {
+                Debug.Log("Raycast");
                 if (hit.collider.gameObject.CompareTag("3DModel"))
                 {
+                    Debug.Log("Trigger");
                     hit.collider.gameObject.GetComponent<Animator>().SetTrigger("isTouched");
                 }
             }
